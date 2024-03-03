a89b47ab987fd6693c8db5556919d90a571e9587
Small__Mushroom_OFFICAL <130323680+SmallMushroom-offical@users.noreply.github.com>
2024-03-03 08:56:39 +0000

    change update error notice in linux

--- overrides/Update.sh
+++ overrides/Update.sh
@@ -6,6 +6,7 @@ echo "下载服务器在国外，建议使用霍格沃兹环境下载"
 echo "在更新前，请确保系统中存在wget"
 read -p "请输入要更新的版本的编号：" updateversion
 echo "请确保在更新时关闭了Minecraft"
+echo "如果在更新时窗口突然退出，则可能是更新失败"
 if [ "$updateversion" = "1" ]; then
     bash ./guirequired/Update/update-stable.sh
     cd ./guirequired/Update
