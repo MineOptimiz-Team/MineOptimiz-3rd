2b537f04d5cdac731f3c7a6f4034edf10368db58
Small__Mushroom_OFFICAL <130323680+SmallMushroom-offical@users.noreply.github.com>
2024-03-13 13:38:45 +0000

    Fix Updater

--- overrides/guirequired/Update/update-stable.sh
+++ overrides/guirequired/Update/update-stable.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 cd ./guirequired/Update/
 echo "正在下载更新包中..."
-wget https://nightly.link/SmallMushroom-offical/MineOptimiz/workflows/main/1.19.4-OptiFabric-Stable -O Update.zip || bash error.sh
+wget https://nightly.link/SmallMushroom-offical/MineOptimiz-Next/workflows/main/1.19.4-OptiFabric-Stable -O Update.zip || exit 1
 echo "正在解压更新包..."
 unzip Update.zip
 rm Update.zip
-mv *.zip Update.zip || bash error.sh
-unzip Update.zip -d ./Update || bash error.sh
+mv *.zip Update.zip || exit 1
+unzip Update.zip -d ./Update || exit 1
 cd ..
 cd ..
 echo "正在删除旧的文件..."
 rm -rf config CustomSkinLoader mods resourcepacks
 echo "正在复制新的文件..."
-cp -r "./guirequired/Update/Update/overrides/config" ./ || bash error.sh
-cp -r "./guirequired/Update/Update/overrides/CustomSkinLoader" ./ || bash error.sh
-cp -r "./guirequired/Update/Update/overrides/mods" ./ || bash error.sh
-cp -r "./guirequired/Update/Update/overrides/resourcepacks" ./ || bash error.sh
+cp -r "./guirequired/Update/Update/overrides/config" ./ || exit 1
+cp -r "./guirequired/Update/Update/overrides/CustomSkinLoader" ./ || exit 1
+cp -r "./guirequired/Update/Update/overrides/mods" ./ || exit 1
+cp -r "./guirequired/Update/Update/overrides/resourcepacks" ./ || exit 1
 echo "正在删除缓存..."
 cd ./guirequired/Update/
 rm Update.zip || echo "删除失败！下次更新可能会出问题！"
