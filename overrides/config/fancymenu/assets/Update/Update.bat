627568104d13afb3b5fa123bc0fabaed191356af
Small__Mushroom_OFFICAL <130323680+SmallMushroom-offical@users.noreply.github.com>
2024-03-16 13:52:43 +0000

    Backport

--- overrides/config/fancymenu/assets/Update/Update.bat
+++ overrides/config/fancymenu/assets/Update/Update.bat
@@ -1,8 +1,5 @@
 @echo off
-cd ..
-cd ..
-cd ..
-cd ..
+cd /d %cd%\guirequired\Update
 echo 1.最新稳定版
 echo 2.最新Dev版
 echo 3.手动选择zip更新包（如果是Github Actions打包的更新包则需先解压一次）
@@ -29,11 +26,11 @@ if "%updateversion%"=="3" (
 if not "%updateversion%"=="3" echo 下载完成
 echo 正在解压更新包中...
 if not "%updateversion%"=="3" (
-  .\config\fancymenu\assets\Update\7z\7z.exe x Update.zip
+  .\7z\7z.exe x Update.zip
   del Update.zip
   ren *.zip Update.zip
 )
-.\config\fancymenu\assets\Update\7z\7z.exe x Update.zip -o".\Update"
+.\7z\7z.exe x Update.zip -o".\Update"
 if errorlevel 1 (
   echo 解压失败！
   pause
@@ -42,12 +39,15 @@ if errorlevel 1 (
 echo 解压完成
 set updatepath="%cd%\Update\overrides"
 echo 正在删除旧文件..
+cd ..
+cd ..
 del /s /q config
 del /s /q CustomSkinLoader
 del /s /q mods
 del /s /q resourcepacks
 echo 删除完成
 echo 正在复制更新文件...
+echo %updatepath%
 xcopy %updatepath%\config .\config /E /H /I
 xcopy %updatepath%\CustomSkinLoader .\CustomSkinLoader /E /H /I
 xcopy %updatepath%\mods .\mods /E /H /I
@@ -59,14 +59,10 @@ if errorlevel 1 (
 )
 echo 复制完成！
 echo 正在删除缓存...
-rmdir /s /q Update
-del /s /q Update.zip
-if errorlevel 1 (
-  echo 删除失败！
-  echo 下次更新可能会出问题
-  pause
-  exit /b 1
-)
+cd /d %updatepath%
+cd ..
+cd ..
+start delcache.bat
 echo 完成！
 echo 请重新启动游戏以查看更改
 pause
