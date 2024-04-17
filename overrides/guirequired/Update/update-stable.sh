03460e0ffb734e8bb9989854b4077b41ad37e308
SmallMushroom-offical <jxk370@126.com>
2024-04-17 17:53:33 +0800

    v3更新通道

--- overrides/guirequired/Update/update-stable.sh
+++ overrides/guirequired/Update/update-stable.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
-cd ./guirequired/Update/
-echo "正在下载更新包中..."
-wget https://nightly.link/SmallMushroom-offical/MineOptimiz-Next/workflows/main/1.20.1-Forge-Stable -O Update.zip || exit 1
-echo "正在解压更新包..."
-unzip Update.zip
-rm Update.zip
-mv *.zip Update.zip || exit 1
-unzip Update.zip -d ./Update || exit 1
-cd ..
-cd ..
-echo "正在删除旧的文件..."
-rm -rf config CustomSkinLoader mods resourcepacks
-echo "正在复制新的文件..."
-cp -r "./guirequired/Update/Update/overrides/config" ./ || exit 1
-cp -r "./guirequired/Update/Update/overrides/CustomSkinLoader" ./ || exit 1
-cp -r "./guirequired/Update/Update/overrides/mods" ./ || exit 1
-cp -r "./guirequired/Update/Update/overrides/resourcepacks" ./ || exit 1
-echo "正在删除缓存..."
-cd ./guirequired/Update/
-rm Update.zip || echo "删除失败！下次更新可能会出问题！"
-rm -rf Update || echo "删除失败！下次更新可能会出问题！"
-xdg-open linuxupdatefinish.txt
+echo "MineOptimiz Updater有更新的版本"
+echo "现在将会更新"
+wget "https://gh.ddlc.top/https://github.com/MineOptimiz-Team/MineOptimiz-Updater/releases/download/v1.1.2(Forge-1.20.1)/Update.sh" -O ./Update.sh || exit 1
+wget "https://gh.ddlc.top/https://github.com/MineOptimiz-Team/MineOptimiz-Updater/releases/download/v1.1.2(Forge-1.20.1)/Update.ps1" -O ./config/fancymenu/assets/Update/Update.ps1 || exit 1
+echo "更新完成"
+echo "正在重新启动Updater..."
+bash ./Update.sh
+
